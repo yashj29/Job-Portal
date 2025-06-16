5f9b7deae7bfd5fe41e7db2f52684a0bbe81ef09
yashj29 <29yashjain29@gmail.com>
2025-06-17 00:04:41 +0530

    Navbar with Clerk Auth Completed

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,15 +1,30 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { useClerk, UserButton, useUser } from '@clerk/clerk-react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+
+  const { openSignIn } = useClerk()
+  const { user } = useUser()
   return (
     <div className='shadow py-4'>
       <div className='container px-4 2xl:px-20 mx-auto flex justify-between items-center'>
         <img src={assets.logo} alt="" />
-        <div className='flex gap-4 max-sm:text-xs'>
+        {
+          user
+          ?<div className='flex items-center gap-3'>
+            <Link to={'/applications'}>Applied Jobs</Link>
+            <p>|</p>
+            <p>Hi, {user.firstName+" "+user.lastName}</p>
+            <UserButton />
+          </div>
+          :<div className='flex gap-4 max-sm:text-xs'>
           <button className='text-gray-600'>Recruiter Login</button>
-          <button className='bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full'>Login</button>
+          <button onClick={e => openSignIn()} className='bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full'>Login</button>
         </div>
+        }
+        
       </div>
     </div>
   )
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,21 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { AppContextProvider } from './context/AppContext.jsx'
+import { ClerkProvider } from '@clerk/clerk-react'
+
+// Import your Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <AppContextProvider>
-      <App />
-    </AppContextProvider>
-  </BrowserRouter>,
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+    <BrowserRouter>
+      <AppContextProvider>
+        <App />
+      </AppContextProvider>
+    </BrowserRouter>
+  </ClerkProvider>,
 )
